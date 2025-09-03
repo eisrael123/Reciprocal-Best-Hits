07bd20f056692bb250080cac78de7c56bea3d466
Ethan Israel <ewi@login1.talapas.uoregon.edu>
2025-09-02 20:09:42 -0700

    finished assignment

--- 1_1alignment.py
+++ 1_1alignment.py
@@ -40,37 +40,46 @@ biomart_species1 = {}
 biomart_species2 = {}
 with open(args.biomart1, 'r') as f_in1, open(args.biomart2, 'r') as f_in2:
     for line in f_in1:
-        columns = line.strip().split('\t')
-        if len(columns) != 3:
-            continue
-        gene_id, protein_id, gene_name = columns
-        biomart_species1[protein_id] = [gene_id, gene_name]
+        columns = line.strip().split()
+        if len(columns) == 2: #only contains protein ID and gene ID
+            gene_id, protein_id = columns
+            biomart_species1[protein_id] = [gene_id, ""]
+        elif len(columns) == 3: #contains all information
+            gene_id, protein_id, gene_name = columns
+            biomart_species1[protein_id] = [gene_id, gene_name]
 
     for line in f_in2:
-        columns = line.strip().split('\t')
-        if len(columns) != 3:
-            continue
-        gene_id, protein_id, gene_name = columns
-        biomart_species2[protein_id] = [gene_id, gene_name]
+        columns = line.strip().split()
+        if len(columns) == 2: 
+            gene_id, protein_id = columns
+            biomart_species2[protein_id] = [gene_id, ""]
+        elif len(columns) == 3:
+            gene_id, protein_id, gene_name = columns
+            biomart_species2[protein_id] = [gene_id, gene_name]
 
 #print(biomart_species1)
 #print(biomart_species2)
 
 species1_hits = {}
 species2_hits = {}
+duplicates_species_1 = 0
+duplicates_species_2 = 0
+not_sorted_correctly_1 = 0
+not_sorted_correctly_2 = 0
 
 with open(args.blast1, 'r') as f_in1, open(args.blast2, 'r') as f_in2:
     # Process BLAST results for species 1
     skipped_gene = ""
     for line in f_in1:
-        columns = line.strip().split('\t')
+        columns = line.strip().split()
         # Ensure the line has enough columns to prevent index errors
-        if len(columns) < 11:
+        if len(columns) != 12:
+            print("columns aren't equal to 12")
             continue
         
         gene1 = columns[0]
-        if gene1 == skipped_gene:
-            continue
+        # if gene1 == skipped_gene:
+        #     continue
         
         gene2 = columns[1]
         evalue = float(columns[10])
@@ -80,25 +89,38 @@ with open(args.blast1, 'r') as f_in1, open(args.blast2, 'r') as f_in2:
             species1_hits[gene1] = [[gene2], [evalue]]
         else:
             # This gene already has a recorded best hit
-            if evalue == species1_hits[gene1][1][0] and gene2 not in species1_hits[gene1][0]:
+            if evalue == species1_hits[gene1][1][0]:
+                if gene2 in species1_hits[gene1][0]:
+                    duplicates_species_1 += 1
+                    continue
                 # Tie for the best hit (but not a duplicate), add this hit as well.
                 species1_hits[gene1][0].append(gene2)
                 species1_hits[gene1][1].append(evalue)
+            elif evalue < species1_hits[gene1][1][0]:
+                print("NOT SORTED CORRECTLY")
+                print(f"best hit: {gene1}, {species1_hits[gene1][0][0]}, {species1_hits[gene1][1][0]}")
+                print(f"current hit: {gene1}, {gene2}, {evalue}")
+                not_sorted_correctly_1 += 1 
+                #break
             else:
+                skipped_gene = gene1
+                #print(f"{gene1} and {gene2} not sorted correctly")
                 # This hit is worse than the best one found so far.
                 # Since the file is sorted, all subsequent hits for this gene are also worse.
-                skipped_gene = gene1
+                
+    print(f"{not_sorted_correctly_1} genes not sorted correctly in file 1")
     
     # Process BLAST results for species 2
     skipped_gene = ""
     for line in f_in2:
-        columns = line.strip().split('\t')
-        if len(columns) < 11:
+        columns = line.strip().split()
+        if len(columns) != 12:
+            print("columns aren't equal to 12")
             continue
 
         gene1 = columns[0]
-        if gene1 == skipped_gene:
-            continue
+        # if gene1 == skipped_gene:
+        #     continue
 
         gene2 = columns[1]
         evalue = float(columns[10])
@@ -106,33 +128,52 @@ with open(args.blast1, 'r') as f_in1, open(args.blast2, 'r') as f_in2:
         if gene1 not in species2_hits:
             species2_hits[gene1] = [[gene2], [evalue]]
         else:
-            if evalue == species2_hits[gene1][1][0] and gene2 not in species2_hits[gene1][0]:
+            if evalue == species2_hits[gene1][1][0]:
+                if gene2 in species2_hits[gene1][0]:
+                    duplicates_species_2 += 1
+                    continue
                 species2_hits[gene1][0].append(gene2)
                 species2_hits[gene1][1].append(evalue)
+            elif evalue < species2_hits[gene1][1][0]:
+                print("NOT SORTED CORRECTLY")
+                print(f"best hit: {gene1}, {species2_hits[gene1][0][0]}, {species2_hits[gene1][1][0]}")
+                print(f"current hit: {gene1}, {gene2}, {evalue}")
+                not_sorted_correctly_2 += 1 
+                #break
             else:
                 skipped_gene = gene1
 
-#print(species1_hits)
-#print(species2_hits)
+    print(f"{not_sorted_correctly_2} genes not sorted correctly in file 2")
+
+# for gene_species_1,value in species1_hits.items():
+#     print(f"{gene_species_1}")
+#     print(f"{value}\n")
+print(f"{duplicates_species_1} duplicates found in species 1")
+print(f"{duplicates_species_2} duplicates found in species 2")
+# print(species1_hits)
+# print(species2_hits)
 
 reciprocal_count = 0
 not_found_count = 0
+not_exist_in_2 = 0
 with open(args.output, 'w') as f_out:
     # Write a header for the output file
     header = (
-            "Species 1 Gene ID\tSpecies 1 Protein ID\tSpecies 1 Gene Name\t"
-            "Species 2 Gene ID\tSpecies2 Protein ID\tSpecies 2 Gene Name\n"
+            "Species1GeneID\tSpecies1ProteinID\tSpecies1GeneName\t"
+            "Species2GeneID\tSpecies2ProteinID\tSpecies2GeneName\n"
         )
     f_out.write(header)
     for gene_species_1, value in species1_hits.items():
         # Condition 1: Check if there's only one unique best hit for the gene from species 1
         if len(value[0]) != 1:
             continue
-        
+
         gene_species_2 = value[0][0]
 
         # Check if the best hit from species 1 even exists as a query in species 2's results
         if gene_species_2 not in species2_hits:
+            #print(f"{gene_species_2} doesn't exist in species 2")
+            not_exist_in_2 += 1
             continue
 
         # Condition 2: Check if there's only one unique best hit for that corresponding gene in species 2
@@ -149,11 +190,11 @@ with open(args.output, 'w') as f_out:
 
             # Skip if we can't find the annotation for either protein
             if not info1:
-                print(f"info not found for {gene_species_1}, skipping...")
+                print(f"Best hit is ({gene_species_1},{gene_species_2}), but info not found for {gene_species_1}, skipping...")
                 not_found_count += 1
                 continue
             if not info2:
-                print(f"info not found for {gene_species_2}, skipping...")
+                print(f"Best hit is ({gene_species_1},{gene_species_2}), but info not found for {gene_species_2}, skipping...")
                 not_found_count += 1
                 continue
 
@@ -173,7 +214,8 @@ with open(args.output, 'w') as f_out:
             )
             f_out.write(output_line)
             reciprocal_count += 1
-print(f"{not_found_count} hits not found in the biomart dicitonary")
-print(f"Found and wrote {reciprocal_count} reciprocal best hit pairs to {args.output}.")
-print("\nDone.")
+print(f"{not_exist_in_2} best hits for a protein does not exist in the other blast file")
+print(f"{not_found_count} hits not found in the biomart dictionary")
+print(f"Found and wrote {reciprocal_count} reciprocal best hit pairs.")
+print("Done.")
 
--- 1_1alignment.sh
+++ 1_1alignment.sh
@@ -8,15 +8,60 @@
 #SBATCH --error=error_%j.log     # STDERR
 set -e 
 
+## MY TEST 
 # blast1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/test_file_1.txt"
 # blast2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/test_file_2.txt"
 # output="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/test_output.tsv"
 
+##JOSH TEST
+# blast1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/test_files_josh/a_blastp.tsv"
+# blast2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/test_files_josh/b_blastp.tsv"
+# biomart1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/test_files_josh/a_biomart.txt"
+# biomart2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/test_files_josh/b_biomart.txt"
+# output="test_output.tsv"
 
-blast1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_human_against_zfish.txt"
-blast2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_zfish_against_human.txt"
-biomart1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/human_biomart.tsv"
-biomart2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/zebrafish_biomart.tsv"
-output="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/1to1_human_zebrafish.tsv"
+##HUMAN ZEBRAFISH
+# blast1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_human_against_zfish.txt"
+# blast2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_zfish_against_human.txt"
+# biomart1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/hsa_114_biomart.tsv"
+# biomart2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/dre_114_biomart.tsv"
+# output="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/1to1_human_zebrafish.tsv"
 
-/usr/bin/time -v ./1_1alignment.py --blast1 $blast1 --blast2 $blast2 --biomart1 $biomart1 --biomart2 $biomart2 -o $output
+##HUMAN EEL 
+# blast1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_human_against_eel.txt"
+# blast2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_eel_against_human.txt"
+# biomart1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/hsa_114_biomart.tsv"
+# biomart2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/eel_114_biomart.tsv"
+# output="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/1to1_human_eel.tsv"
+
+##HUMAN BABYWHALE
+# blast1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_human_against_babywhale.txt"
+# blast2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_babywhale_against_human.txt"
+# biomart1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/hsa_114_biomart.tsv"
+# biomart2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/pka_114_biomart.tsv"
+# output="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/1to1_human_babywhale.tsv"
+
+##ZEBRAFISH EEL 
+# blast1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_zfish_against_eel.txt"
+# blast2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_eel_against_zfish.txt"
+# biomart1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/dre_114_biomart.tsv"
+# biomart2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/eel_114_biomart.tsv"
+# output="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/1to1_zfish_eel.tsv"
+
+##ZEBRAFISH BABYWHALE
+# blast1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_zfish_against_babywhale.txt"
+# blast2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_babywhale_against_zfish.txt"
+# biomart1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/dre_114_biomart.tsv"
+# biomart2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/pka_114_biomart.tsv"
+# output="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/1to1_zfish_babywhale.tsv"
+
+#EEL BABYWHALE
+# blast1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_eel_against_babywhale.txt"
+# blast2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/sorted_babywhale_against_eel.txt"
+# biomart1="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/eel_114_biomart.tsv"
+# biomart2="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/biomart/pka_114_biomart.tsv"
+# output="/projects/bgmp/ewi/bioinfo/Bi623/Assignments/Reciprocal-Best-Hits/1to1_eel_babywhale.tsv"
+
+
+#/usr/bin/time -v ./1_1alignment.py --blast1 $blast1 --blast2 $blast2 --biomart1 $biomart1 --biomart2 $biomart2 -o $output
+./1_1alignment.py --blast1 $blast1 --blast2 $blast2 --biomart1 $biomart1 --biomart2 $biomart2 -o $output
